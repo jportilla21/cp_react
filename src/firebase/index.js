0acb7c5aed69b04e7f57b708dcde0ff64b2e205b
Catherine Portilla <cptrujillo015@hotmail.com>
2023-12-22 11:18:16 -0500

    capturas

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,17 +1,3 @@
-// import firebase from 'firebase/compat/app';
-// import 'firebase/compat/firestore';
-// import firebaseConfig from '../config/firebase.config';
-
-// const app = firebase.initializeApp(firebaseConfig);
-
-// export const getFirebase = () => {
-//   return app;
-// };
-
-// export const getFirestore = () => {
-//   return firebase.firestore(app);
-// };
-
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import firebaseConfig from '../config/firebase.config';
